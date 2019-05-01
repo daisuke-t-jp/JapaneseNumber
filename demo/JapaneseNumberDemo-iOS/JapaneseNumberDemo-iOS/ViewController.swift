99464fa3f3090c69565c74e7cdcc4912746c9e4a
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-05-01 09:36:50 +0900

    Demo: Add iOS

---
+++ demo/JapaneseNumberDemo-iOS/JapaneseNumberDemo-iOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  JapaneseNumberDemo-iOS
+//
+//  Created by Daisuke T on 2019/05/01.
+//  Copyright © 2019 DaisukeT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    Common.test()
+  }
+
+
+}
+
